8aaeb9675010a1be42975683a015afc08a04fe68
dreamqip <v.chuchupak@gmail.com>
2023-10-11 15:25:41 +0200

    feat: add support for svg icons

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,8 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -8,9 +11,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  */
 const config = {
   resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
     unstable_enablePackageExports: true,
     resolverMainFields: ['react-native', 'browser', 'main'],
   },
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, config);
