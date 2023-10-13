a0e1289d331f64691f261e54bf9c955648d469a1
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-10-13 18:36:25 +0200

    feat: introduce umi provider

--- App.tsx
+++ App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
 
 import { ROUTES } from './src/constants/routes';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TabNavigation from './src/components/tab-navigator';
+import UmiProvider from './src/providers/umi-provider';
 
 const Stack = createNativeStackNavigator();
 
 export default function App(): JSX.Element {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name={ROUTES.TAB.INDEX} component={TabNavigation} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <UmiProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name={ROUTES.TAB.INDEX} component={TabNavigation} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </UmiProvider>
   );
 }
