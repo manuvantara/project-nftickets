a9e4c31087ff125ae4e050b07306a302cb50c471
Vladyslav Chuchupak <87983911+dreamqip@users.noreply.github.com>
2023-10-13 13:23:56 +0200

    feat: add application routes (#3)

--- App.tsx
+++ App.tsx
@@ -2,42 +2,23 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 
-import AddCircle from './src/images/AddCircle.svg';
+import { ROUTES } from './src/constants/routes';
 
-import { AUTHORITY_PRIVATE_KEY } from '@env';
-import { initializeUmi } from './src/utils/metaplex/core';
-import {
-  createSignerFromKeypair,
-  keypairIdentity,
-} from '@metaplex-foundation/umi';
-
-// initialize umi
-const umi = initializeUmi();
-// initialize signer
-const authorityKeypair = umi.eddsa.createKeypairFromSecretKey(
-  new Uint8Array(JSON.parse(AUTHORITY_PRIVATE_KEY)),
-);
-const authority = createSignerFromKeypair(umi, authorityKeypair);
-umi.use(keypairIdentity(authority));
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontFamily: 'Montserrat-SemiBold' }}>Home Screen</Text>
-      <AddCircle width={120} height={120} />
-    </View>
-  );
-}
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import TabNavigation from './src/components/tab-navigator';
 
 const Stack = createNativeStackNavigator();
 
 export default function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name={ROUTES.TAB.INDEX} component={TabNavigation} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
---
+++ src/components/screens/available-events.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function AvailableEventsScreen() {
+  return (
+    <View>
+      <Text>AvailableEventsScreen</Text>
+    </View>
+  );
+}
+
+const s = StyleSheet.create({});
