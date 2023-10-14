b7f528f591e9b9f48f50f7ff8a675b54c57bed6e
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-10-14 03:36:46 +0200

    feat: load user events by respective tickets

--- app/components/screens/my-tickets.tsx
+++ app/components/screens/my-tickets.tsx
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
-import EventCard from '../event';
-
-const MOCK_EVENT = {
-  cover: 'https://picsum.photos/200',
-  title: 'Atlass Weekend 2023',
-  date: '2023-04-23',
-};
+import EventCard, { EventCard as EventCardProps } from '../event';
+import useUmi from '../../hooks/use-umi';
+import {
+  fetchMetadatasByUris,
+  fetchTicketEventPairsByOwner,
+  fetchUrisByMintList,
+} from '../../utils/metaplex/nft-retrieval';
+import { NftMetadata } from '../../utils/types';
 
 export default function MyTicketsScreen() {
+  const [events, setEvents] = useState<EventCardProps[]>();
+  const umi = useUmi();
+
+  useEffect(() => {
+    async function getMyTickets() {
+      try {
+        const ticketEventPairs = await fetchTicketEventPairsByOwner(umi);
+        if (!ticketEventPairs) return;
+
+        const eventUris = await fetchUrisByMintList(
+          umi,
+          ticketEventPairs.events,
+        );
+        if (!eventUris) return;
+
+        const eventMetadatas = await fetchMetadatasByUris(eventUris);
+        if (!eventMetadatas) return;
+
+        const events = eventMetadatas.map(
+          (eventMetadata: NftMetadata, index: number) => ({
+            cover: eventMetadata.image,
+            title: eventMetadata.name,
+            date: Number(eventMetadata.attributes?.[0]?.value) * 1000 ?? 0,
+            publicKey: ticketEventPairs.events[index],
+          }),
+        );
+        const filteredEvents = events.filter(event => event.title !== '');
+
+        setEvents(filteredEvents);
+      } catch (error) {
+        console.error('Error fetching my tickets:', error);
+      }
+    }
+    getMyTickets();
+  }, []);
+
   return (
     <View style={s.container}>
       <FlatList
-        data={[MOCK_EVENT, MOCK_EVENT, MOCK_EVENT]}
+        data={events}
         contentContainerStyle={s.listContainer}
         style={s.list}
         ItemSeparatorComponent={() => <View style={{ height: 24 }} />}
