aa58cc0ec6759acfbb3f3eaa57fa1e598ab1308b
YevheniiDimov <83459259+YevheniiDimov@users.noreply.github.com>
2023-10-14 12:10:45 +0100

    fix: get rid of src folder

--- src/components/screens/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MyTicketsScreen } from './my-tickets';
-export { default as AvailableEventsScreen } from './available-events';
-export { default as MyEventsScreen } from './my-events';
--- src/hooks/use-umi.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { UmiContext } from '../providers/umi-provider';
-
-export default function useUmi() {
-  const context = useContext(UmiContext);
-  if (!context) throw new Error('useUmi must be used within a UmiProvider');
-  return context;
-}
